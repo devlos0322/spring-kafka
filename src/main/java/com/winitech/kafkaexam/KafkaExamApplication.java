8242b8a29da14947ecc47aa87dc87882100001f1
devlos0322 <devlos0322@gmail.com>
2021-08-18 13:55:16 +0900

    Add springboot with kafka

---
+++ src/main/java/com/winitech/kafkaexam/KafkaExamApplication.java
@@ -0,0 +1,12 @@
+package com.winitech.kafkaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 참고: https://victorydntmd.tistory.com/348
+@SpringBootApplication
+public class KafkaExamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExamApplication.class, args);
+    }
+}
---
+++ src/main/java/com/winitech/kafkaexam/Service/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.winitech.kafkaexam.Service;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class KafkaConsumer {
+    @KafkaListener(topics = "exam", groupId = "foo")
+    public void consume(String message) throws IOException {
+        System.out.println(String.format("Consumed message : %s", message));
+    }
+}
